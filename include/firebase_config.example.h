c6c41b7a143e7ddc129282698bc4231d42355c87
Ravi Maurya <craftmaster17488@gmail.com>
2026-08-07 20:15:16 +0530

    Ignore locan Firebase config and add config templete

---
+++ include/firebase_config.example.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Firebase Project Configuration
+#define FIREBASE_API_KEY        "api_key_here"
+#define FIREBASE_DATABASE_URL   "database_url_here"
+#define DEVICE_ID               "device001"
--- include/firebase_config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Firebase Project Configuration
-#define API_KEY        "api_key_here"
-#define DATABASE_URL   "database_url_here"
-#define DEVICE_ID      "device001"
